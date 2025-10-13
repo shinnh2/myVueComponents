3d095be75f91b2dbad19993f9bf8e10a8817ac75
신나현 <nahyeon.shin@hnine.com>
2025-10-13 16:31:38 +0900

    Badge 컴포넌트 로직 수정

--- src/components/badge/Badge.vue
+++ src/components/badge/Badge.vue
@@ -1,50 +1,49 @@
 <script setup>
-import { reactive, computed } from "vue";
+  import { reactive, computed } from "vue";
 
-/**
- * Badge 컴포넌트 Props 정의
- * @property {String} size - 버튼 크기
- * @property {String} variant - 배지 스타일 (외곽선, 강조 등등)
- */
-const rawProps = defineProps({
-  size: {
-    type: String,
-    validator(value, props) {
-      return ["sm", "md"].includes(value);
+  /**
+   * Badge 컴포넌트 Props 정의
+   * @property {String} size - 버튼 크기
+   * @property {String} variant - 배지 스타일 (에러, 에러 강조, 외곽선 등등)
+   */
+  const rawProps = defineProps({
+    size: {
+      type: String,
+      validator(value, props) {
+        return ["sm", "md"].includes(value);
+      },
+      default: "md",
     },
-    default: "md",
-  },
-  variant: {
-    type: String,
-    validator(value, props) {
-      return ["", "error", "error-em", "contained"].includes(value);
+    variant: {
+      type: String,
+      validator(value, props) {
+        return ["", "error", "error-em", "contained"].includes(value);
+      },
+      default: "",
     },
-    default: "",
-  },
-});
-const props = reactive(rawProps);
+  });
+  const props = reactive(rawProps);
 
-/**
- * 버튼 클래스명을 조합하여 정의하는 연산 함수
- */
-const badgeClass = computed(() => ({
-  badge: true,
-  [`badge--${props.size}`]: !!props.size,
-  [`badge--${props.variant}`]: !!props.variant,
-}));
+  /**
+   * 버튼 클래스명을 조합하여 정의하는 연산 함수
+   */
+  const badgeClass = computed(() => ({
+    badge: true,
+    [`badge--${props.size}`]: !!props.size,
+    [`badge--${props.variant}`]: !!props.variant,
+  }));
 </script>
 
 <template>
   <div v-if="$slots.default" class="badge" :class="badgeClass">
     <slot name="default">기본 배지</slot>
   </div>
-
-  <sup v-if="$slots.superscipt && $slots.superscipt().length" class="badge badge--sup">
+  <sup v-else-if="$slots.superscipt && $slots.superscipt().length" class="badge badge--sup">
     <slot name="superscipt">위 첨자 스타일 배지</slot>
   </sup>
   <sup v-else class="badge badge--sup badge--dot" aria-label="알림 표시"></sup>
 </template>
 
 <style scoped>
-@import "./Badge.scss";
+  @import "./Badge.scss";
 </style>
